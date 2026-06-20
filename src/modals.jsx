71dded35a8115c90e4c1f11f16be81bc1ec6cdd8
Hira Khyzer <55583242+Hirakhyzer@users.noreply.github.com>
2026-06-20 11:41:47 +0800

    Add dialog exports

---
+++ src/modals.jsx
@@ -0,0 +1,5 @@
+export { ClientModal } from "./clientModal";
+export { EmailModal } from "./emailModal";
+export { InvoiceModal } from "./invoiceModal";
+export { EventModal } from "./eventModal";
+export { GoalsModal } from "./goalsModal";
