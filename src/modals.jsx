22f54646b71623087e4b88ed092931ed897e8773
Hira Khyzer <55583242+Hirakhyzer@users.noreply.github.com>
2026-06-20 11:43:52 +0800

    Use billing dialog export

--- src/modals.jsx
+++ src/modals.jsx
@@ -1,5 +1,5 @@
 export { ClientModal } from "./clientModal";
 export { EmailModal } from "./emailModal";
-export { InvoiceModal } from "./invoiceModal";
+export { BillingModal as InvoiceModal } from "./billingModal";
 export { EventModal } from "./eventModal";
 export { GoalsModal } from "./goalsModal";
